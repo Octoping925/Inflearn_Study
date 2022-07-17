263cfbc66b0addddd151631bdfb6f3c09f57ff90
Octoping925 <mooner925@gmail.com>
2022-07-18 08:08:14 +0900

    스프링 웹 개발 기초 - API @ResponseBody가 붙어있을 경우에는, viewResolver에 값을 던지는 것이 아니라 HTTP의 Body에 그대로 문자열을 던짐. 객체가 return 됐을 경우에는, HttpMessageConverter에서 변환 후 던진다.

--- src/main/java/octoping/mycspring/controller/HelloController.java
+++ src/main/java/octoping/mycspring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,9 +14,39 @@ public class HelloController {
         return "hello";
     }
 
+    // MVC 방식
+    // MVC가 templateSample.html을 조작해서 웹 페이지에 출력함
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "templateSample";
+    }
+
+    // API 방식 - 문자열 넘기기
+    // HTML 없이 그냥 문자열만 웹 페이지에 출력됨
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    // API 방식 - 객체 넘기기
+    // 객체의 JSON 형식 문자열이 웹 페이지에 출력됨
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
