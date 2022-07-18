45af766e41c267a8d735a44f91cb6718754e2418
Octoping925 <mooner925@gmail.com>
2022-07-18 19:23:46 +0900

    회원 관리 예제-백엔드 개발 - 회원 서비스 개발 Service에서는 비즈니스에 가까운 이름을 사용해야 함.
    Optional로 묶인 값의 경우에는 ifPresent를 이용해 null 예외를 핸들링할 수 있음

---
+++ src/main/java/octoping/mycspring/service/MemberService.java
@@ -0,0 +1,36 @@
+package octoping.mycspring.service;
+
+import java.util.List;
+import java.util.Optional;
+import octoping.mycspring.domain.Member;
+import octoping.mycspring.repository.MemberRepository;
+import octoping.mycspring.repository.MemoryMemberRepository;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // 같은 이름의 중복 회원 X
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
