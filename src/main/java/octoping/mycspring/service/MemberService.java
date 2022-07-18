59fa3b2beb07a8158dd47c2ec8799ee0b9105f78
Octoping925 <mooner925@gmail.com>
2022-07-18 19:47:21 +0900

    회원 관리 예제-백엔드 개발 - 회원 서비스 테스트 클래스에서 Ctrl Shift T를 누르면 테스트 코드를 빠르게 생성할 수 있음.
    1. Ctrl Shift V를 누르면 작성한 코드를 변수에 대입할 수 있음.
    2. 테스트 코드를 작성할 땐 given when then 기법을 활용하면 좋음
    3. Assertions.fail 함수를 이용하면 테스트를 실패시킬 수 있음
    4. 예외가 발생해야 할 땐 assertThrows 사용

--- src/main/java/octoping/mycspring/service/MemberService.java
+++ src/main/java/octoping/mycspring/service/MemberService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 import octoping.mycspring.domain.Member;
 import octoping.mycspring.repository.MemberRepository;
-import octoping.mycspring.repository.MemoryMemberRepository;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // 회원 가입
     public Long join(Member member) {
