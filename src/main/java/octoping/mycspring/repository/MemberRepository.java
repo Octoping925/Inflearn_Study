5e638a28cd1855a29508d8e6b0f5b25c28e0f539
Octoping925 <mooner925@gmail.com>
2022-07-18 12:57:21 +0900

    회원 관리 예제-백엔드 개발 - 회원 도메인과 리포지토리 만들기 회원 도메인과 레포지토리를 구현했고, DB는 아직 정해지지 않았으므로 메모리 멤버 레포지토리를 만들었다

---
+++ src/main/java/octoping/mycspring/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package octoping.mycspring.repository;
+
+import java.util.List;
+import java.util.Optional;
+import octoping.mycspring.domain.Member;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
