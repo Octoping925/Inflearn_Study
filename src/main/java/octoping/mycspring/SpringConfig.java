68ce769b53c68c58be9dec2aefac754c875082c0
Octoping <myc1365@naver.com>
2022-07-31 23:02:40 +0900

    스프링 DB 접근 기술 - 순수 JDBC
    기존의 MemoryMemberRepository에서 JdbcMemberRepository로 리포지토리를 변경하고 싶을 땐, 이전에 만들어두었던 SpringConfig에서 다음과 같이 변경만 해주면 된다.
    스프링의 DI를 사용하면 기존 코드를 전혀 손대지 않고, 설정만으로 구현
    클래스를 변경할 수 있다.

--- src/main/java/octoping/mycspring/SpringConfig.java
+++ src/main/java/octoping/mycspring/SpringConfig.java
@@ -1,13 +1,25 @@
 package octoping.mycspring;
 
+import octoping.mycspring.repository.JdbcMemberRepository;
 import octoping.mycspring.repository.MemberRepository;
 import octoping.mycspring.repository.MemoryMemberRepository;
 import octoping.mycspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
