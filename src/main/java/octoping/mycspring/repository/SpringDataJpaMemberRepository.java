9f8f86fd391a219473ee0c1856a76d3b8a9fe770
Octoping <myc1365@naver.com>
2022-08-03 23:33:42 +0900

    스프링 DB 접근 기술 - 스프링 데이터 JPA
    스프링 데이터 JPA를 사용하면 리포지토리에 구현 클래스 없이 인터페이스만으로 개발을 완료할 수 있음.
    
    스프링 데이터 JPA는 JPA를 편리하게 사용하도록 도와주는 기술이므로, JPA를 먼저 학습한 후에 학습해야 한다.
    
    스프링 데이터 JPA가 SpringDataJpaMemberRepository 를 스프링 빈으로 자동 등록해준다.
    
    인터페이스에 `Optional<Member> findByName(String name);` 와 같이 메소드 이름을 적는 것 만으로도 자동으로 쿼리를 생성해준다.

---
+++ src/main/java/octoping/mycspring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,11 @@
+package octoping.mycspring.repository;
+
+import octoping.mycspring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    @Override
+    Optional<Member> findByName(String name);
+}
