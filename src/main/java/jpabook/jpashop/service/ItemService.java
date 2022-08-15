9b502bd4e3f7abb8ce9d677a74e714b5e3f844be
Octoping925 <myc1365@naver.com>
2022-08-15 18:38:55 +0900

    상품 도메인 개발 - 상품 서비스 개발

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    /**
+     * 상품 추가
+     */
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    /**
+     * 상품 전체 조회
+     */
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    /**
+     * 상품 단건 조회
+     */
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
