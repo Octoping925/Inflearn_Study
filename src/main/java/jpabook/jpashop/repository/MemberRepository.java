2f4a10a3cd499a002350a491058dcf4f5d1393b5
Octoping <myc1365@naver.com>
2022-08-15 15:56:20 +0900

    회원 도메인 개발 - 회원 리포지토리 개발
    findAll을 할 땐 em.createQuery로 JPQL을 작성해야 한다.
    JPQL은 SQL과 다르게 테이블을 대상으로 하지 않고 엔티티를 대상으로 한다.
    
    *`@PersistenceContext`*를 달면 Spring이 알아서 em을 주입해준다.

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -5,16 +5,27 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
     @PersistenceContext
-    EntityManager em;
-    public Long save(Member member) {
+    private EntityManager em;
+
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+    public List<Member> findAll() {
+        return em.createQuery("SELECT m FROM Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("SELECT m FROM Member m WHERE m.username = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
