0b5f584051755045337d35512456a66612a30965
Octoping925 <myc1365@naver.com>
2022-08-15 17:55:48 +0900

    회원 도메인 개발 - 회원 기능 테스트
    em.persist를 한다고 해서 insert가 되는 것이 아니다.
    
    Transaction이 커밋 될 때 영속성 컨텍스트가 flush되며 날아가는 것이기 때문.
    
    그래서 이 테스트는 Transactional 때문에 커밋이 안되는 코드이므로 insert가 날아갈 필요도 굳이 없는 것
    
    테스트 중 예외가 발생해야 할 경우 @Test(expected = IllegalStateException.class)를 써주면 깔끔
    
    그리고 테스트를 할 땐 아무래도 병렬적으로 돌아갈 일도 많고 하기 때문에 main의 DB보다는 따로 DB를 만들어서 쓰는 것이 좋다.
    
    H2는 메모리 DB를 지원하기 때문에 이를 사용해보자.
    
    main에는 resources 폴더가 있어서 그 곳의 application.yml을 참조하지만,
    
    test에도 resources 폴더를 만들면 테스트할 때 이 resources를 먼저 참조하게 된다.
    
    test에 resources/application.yml을 만들어서 url을 jdbc:h2:mem:test 로 하면 H2의 메모리 DB를 테스트 용도로 사용할 수 있다.
    
    그리고 스프링부트가 별 다른 설정이 없으면 메모리 DB로 돌리기 때문에 application.yml을 그냥 비워놔도 극단적으로는 큰 상관 없다.

---
+++ src/test/java/jpabook/jpashop/MemberServiceTest.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.service.MemberService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        Member member2 = new Member();
+        member1.setUsername("kim");
+        member2.setUsername("kim");
+
+        // when
+        memberService.join(member1);
+        memberService.join(member2); // 예외 발생
+
+        // then
+        fail("예외가 발생해야 한다");
+    }
+    
+}
